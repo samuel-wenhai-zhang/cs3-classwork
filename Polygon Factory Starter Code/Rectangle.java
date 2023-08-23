9b3ba2bfc085d7443ef2a3296a1dd27e1c3bbefb
Boltcrasher <Boltcrasher@gmail.com>
2023-08-23 13:31:17 -0500

    Update Rectangle.java

--- Polygon Factory Starter Code/Rectangle.java
+++ Polygon Factory Starter Code/Rectangle.java
@@ -1,4 +1,16 @@
-public class Rectangle {
+public class Rectangle implements Polygon {
+    private double length;
+    private double width;
 
-    
+    public double area() {
+        return length * width;
+    }
+
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+
+    public String getType() {
+        return "Rectangle";
+    }
 }
