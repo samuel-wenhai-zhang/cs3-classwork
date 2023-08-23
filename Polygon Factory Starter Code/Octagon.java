ba3f4a43416604603f43d9a5ba4e9de53eb30c7b
Boltcrasher <137728403+Boltcrasher@users.noreply.github.com>
2023-08-23 15:05:09 -0500

    Update Octagon.java

--- Polygon Factory Starter Code/Octagon.java
+++ Polygon Factory Starter Code/Octagon.java
@@ -1,4 +1,10 @@
-public class Octagon {
+public class Octagon extends RegularNGon{
+    public Octagon(double s) {
+        super(8, s);
+    }
 
-    
+    @Override
+    public String getType() {
+        return "Octagon";
+    }
 }
