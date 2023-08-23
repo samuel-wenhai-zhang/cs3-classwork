83dc86b65eab43bd7c6fdfa5ce7bf3e2c4572a52
Boltcrasher <137728403+Boltcrasher@users.noreply.github.com>
2023-08-23 15:14:44 -0500

    Update PolygonFactory.java

--- Polygon Factory Starter Code/PolygonFactory.java
+++ Polygon Factory Starter Code/PolygonFactory.java
@@ -1,9 +1,14 @@
+import java.util.*;
+import java.io.*;
+
 public class PolygonFactory {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("**********************************************************\n");
         System.out.println("Welcome to the Polygon Factory!\n");
         System.out.println("**********************************************************\n");
 
         // your code here
+        Scanner input = new Scanner(System.in);
+        ArrayList<Polygon> polygons = new ArrayList<Polygon>();
     }
 }
