064a843da82562f3e8d44fa5ca42cd779591b15d
Boltcrasher <137728403+Boltcrasher@users.noreply.github.com>
2023-08-23 14:58:21 -0500

    Update RegularNGon.java

--- Polygon Factory Starter Code/RegularNGon.java
+++ Polygon Factory Starter Code/RegularNGon.java
@@ -1,3 +1,17 @@
-public class RegularNGon {
+public class RegularNGon implements Polygon{
 
+  private double side;
+  private double num;
+  
+  public double area() {
+    return n * s * s * 4 * Math.tan(Math.toRadians(180/num))
+  }
+
+  public double perimeter() {
+    
+  }
+
+  public String getType() {
+    return "Regular Polygon";
+  }
 }
