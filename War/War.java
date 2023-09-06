491bdf0a5dcbca97b3a9e49da7af90e58ae4414d
Boltcrasher <137728403+Boltcrasher@users.noreply.github.com>
2023-09-06 19:46:31 +0000

    stuff

--- War/War.java
+++ War/War.java
@@ -1,40 +1,62 @@
+/*
+ * Fewest number of rounds to reach a winner: 
+ * Greatest number of rounds to reach a winner: 
+ */
+
 import java.util.ArrayList;
 
 public class War {
 
-    public static void main(String[] args) {
+    private int round;
+    private ArrayList<Card> d1, d2;
+
+    public War() {
+        round = 1;
         Deck d = new Deck();
-        ArrayList<Card> d1 = new ArrayList<Card>(), d2 = new ArrayList<Card>();
-        divideDeck(d, d1, d2);   
-
-        int round = 1;
-        System.out.println("Round: " + round);
-        playRound(d1, d2);
-        round++;
-        // while (!isGameOver()) {
-
-        // }
-    }
-
-    public static void divideDeck(Deck d, ArrayList<Card> d1, ArrayList<Card> d2) {
         for (int i = 0; i < 26; i++) {
             d1.add(d.drawCard());
             d2.add(d.drawCard());
         }
     }
 
-    public static void playRound(ArrayList<Card> d1, ArrayList<Card> d2) {
-        Card c1 = d1.remove(1), c2 = d2.remove(1);
-        if (c1.compareTo(c2) < 0) {
-            System.out.println(c1 + " loses to " + c2);
-            
+    public static void main(String[] args) {
+        War w = new War();
+
+        while (!w.isGameOver()) {
+            w.playRound();
         }
-        else {
-            System.out.println(c1 + " beats " + c2);
-        }
+        
+        w.gameOver();
+
     }
 
-    public static boolean isGameOver(ArrayList<Card> d1, ArrayList<Card> d2) {
+    public void playRound() {
+        System.out.println("Round " + round + ": ");
+        round++;
+
+        Card c1 = d1.remove(0), c2 = d1.remove(0), c3 = d2.remove(0), c4 = d2.remove(0);
+        if (c2.compareTo(c4) < 0) {
+            System.out.println("Player 1's " + c2 + " loses to Player 2's " + c4);
+            d2.add(c4); d2.add(c3); d2.add(c2); d2.add(c1);
+        }
+        else {
+            System.out.println("Player 1's " + c2 + " beats Player 2's " + c4);
+            d1.add(c2); d1.add(c1); d1.add(c4); d1.add(c3); 
+        }
+        System.out.println("Player 1 has " + d1.size() + " cards left.");
+        System.out.println("Player 2 has " + d2.size() + " cards left.\n");
+    }
+
+    public boolean isGameOver() {
         return d1.size() == 0 || d2.size() == 0;
-    }    
+    }
+
+    public void gameOver() {
+        if (d1.size() == 0) {
+            System.out.println("Player 2 wins!");
+        }
+        else {
+            System.out.println("Player 1 wins!");
+        }
+    }
 }
