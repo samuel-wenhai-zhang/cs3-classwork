785395e89b0647416db091e07db1c0b48a389b1e
Boltcrasher <Boltcrasher@gmail.com>
2023-09-07 12:13:28 -0500

    Update War.java

--- War/War.java
+++ War/War.java
@@ -9,16 +9,16 @@ import java.util.ArrayList;
 public class War {
 
     private int round;
-    private ArrayList<Card> d1, d2;
+    private ArrayList<Card> player1, player2;
 
     public War() {
         round = 1;
-        d1 = new ArrayList<Card>();
-        d2 = new ArrayList<Card>();
+        player1 = new ArrayList<Card>();
+        player2 = new ArrayList<Card>();
         Deck d = new Deck();
         for (int i = 0; i < 26; i++) {
-            d1.add(d.drawCard());
-            d2.add(d.drawCard());
+            player1.add(d.drawCard());
+            player2.add(d.drawCard());
         }
     }
 
@@ -37,25 +37,25 @@ public class War {
         System.out.println("Round " + round + ": ");
         round++;
 
-        Card c1 = d1.remove(0), c2 = d1.remove(0), c3 = d2.remove(0), c4 = d2.remove(0);
+        Card c1 = player1.remove(0), c2 = player1.remove(0), c3 = player2.remove(0), c4 = player2.remove(0);
         if (c2.compareTo(c4) < 0) {
             System.out.println("Player 1's " + c2 + " loses to Player 2's " + c4);
-            d2.add(c4); d2.add(c3); d2.add(c2); d2.add(c1);
+            player2.add(c4); player2.add(c3); player2.add(c2); player2.add(c1);
         }
         else {
             System.out.println("Player 1's " + c2 + " beats Player 2's " + c4);
-            d1.add(c2); d1.add(c1); d1.add(c4); d1.add(c3); 
+            player1.add(c2); player1.add(c1); player1.add(c4); player1.add(c3); 
         }
-        System.out.println("Player 1 has " + d1.size() + " cards left.");
-        System.out.println("Player 2 has " + d2.size() + " cards left.\n");
+        System.out.println("Player 1 has " + player1.size() + " cards left.");
+        System.out.println("Player 2 has " + player2.size() + " cards left.\n");
     }
 
     public boolean isGameOver() {
-        return d1.size() == 0 || d2.size() == 0;
+        return player1.size() == 0 || player2.size() == 0;
     }
 
     public void gameOver() {
-        if (d1.size() == 0) {
+        if (player1.size() == 0) {
             System.out.println("Player 2 wins!");
         }
         else {
