37777f5781979254f1c52db3d7302f406f59eb7f
Boltcrasher <Boltcrasher@gmail.com>
2023-09-11 12:15:23 -0500

    Update Polygon.java

--- SortablePolygons/Polygon.java
+++ SortablePolygons/Polygon.java
@@ -1,5 +1,29 @@
-public interface Polygon {
-    public double area();
-    public double perimeter();
-    public String getType();
+public abstract class Polygon implements Comparable<Polygon> {
+    public abstract double area();
+    public abstract double perimeter();
+    public abstract String getType();
+
+    @Override
+    public String toString() {
+        return "Type: " + getType() + ", Area: " + area() + ", " + perimeter();
+    }
+
+    @Override
+    public int compareTo(Polygon o) {
+        if (area() < o.area()) {
+            return -1;
+        }
+        else if (area() > o.area()) {
+            return 1;
+        }
+        else if (perimeter() < o.perimeter()) {
+            return -1;
+        }
+        else if (perimeter() > o.perimeter()) {
+            return 1;
+        }
+        else {
+            return getType().compareTo(o.getType());
+        }
+    }
 }
